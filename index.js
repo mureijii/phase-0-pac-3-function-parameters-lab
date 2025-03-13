f8c8a881e8587f56f0366c3f225a313a4b9684a8
IanMuriithi <ian.muriithi1@student.moringaschool.com>
2025-03-13 15:25:29 +0300

    Modifies index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Function with one parameter
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+  
+  // Function with two parameters
+  function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  // Function with an optional parameter
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+  // Export functions for testing
+  module.exports = {
+    introduction,
+    introductionWithLanguage,
+    introductionWithLanguageOptional,
+  };
+  
